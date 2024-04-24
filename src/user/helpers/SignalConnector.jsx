0f084e28a2b327855fc55ce18d4a3725dc1eea67
danielcbustos <d.bustosr@uniandes.edu.co>
2024-04-23 23:40:47 -0500

    hu receive control messages finished

---
+++ src/user/helpers/SignalConnector.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import * as signalR from "@microsoft/signalr";
+// import { Alerts } from "../pages/Utils";
+import { AlertNotification } from "../../utils/AlertNotification";
+import { REACT_APP_SIGNALR } from "@env";
+
+
+
+export const SignalConnector = (showInfo, userId) => {
+    const urlSignal = REACT_APP_SIGNALR;
+    const [dataSignal, setDataSignal] = useState();
+    const { showToastSuccess } = AlertNotification()
+    let hubConnection;
+    useEffect(() => {
+        const startSignal = () => {
+            console.log("ReceiveMessage====> ", `ReceiveMessage${userId}`);
+            hubConnection = new signalR.HubConnectionBuilder()
+                .withUrl(urlSignal)
+                .withAutomaticReconnect()
+                .build();
+            hubConnection
+                .start()
+                .then(() => {
+                    console.log("Connection started");
+                })
+                .catch((err) => console.log("Error while starting connection: " + err));
+            hubConnection.on(`ReceiveMessage${userId}`, (data) => {
+                setDataSignal(data);
+            });
+        };
+        if (userId !== "" && userId != undefined) {
+            startSignal();
+        }
+    }, [userId]);
+
+    useEffect(() => {
+        if (dataSignal != undefined) {
+            try {
+                if (dataSignal != "") {
+                    var msg = JSON.parse(dataSignal.trim());
+                    const data = JSON.parse(msg);
+                    const title = data.Title;
+                    const Description = data.Description;
+                    showToastSuccess(title, Description);
+                }
+            } catch (error) {
+                console.error("Error parsing JSON:", dataSignal, error);
+            }
+        }
+    }, [dataSignal]);
+
+    return { dataSignal };
+};
