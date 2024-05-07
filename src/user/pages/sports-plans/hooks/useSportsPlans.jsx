a16dc3e6a8e6d203c522d6af9b6b065a9e2ec940
danielcbustos <d.bustosr@uniandes.edu.co>
2024-05-06 22:48:45 -0500

    select sports plans HU finished

--- src/user/pages/sports-plans/hooks/useSportsPlans.jsx
+++ src/user/pages/sports-plans/hooks/useSportsPlans.jsx
@@ -4,7 +4,7 @@ import { AlertNotification } from "../../../../utils/AlertNotification";
 import { API_URL_SERVICE } from "@env";
 // const urlAPI = process.env.REACT_APP_API_URL_SERVICE;
 export const useSportsPlans = () => {
-    const urlAPI = API_URL_SERVICE
+    const urlAPI = "http://192.168.0.6:32772"
     const { showDialogError } = AlertNotification();
     const [mealPlansByUser, setMealPlansByUser] = useState([]);
     const [loadMealPlans, setLoadMealPlans] = useState(true);
@@ -13,7 +13,7 @@ export const useSportsPlans = () => {
     const getSportsPlans = async () => {
         setLoadMealPlans(true);
         const querySportsPlans = {
-            serviceTypes: ["01B50F0D-3226-4DF2-B912-4DA4B37D9BD9"],
+            serviceTypes: ["3040214a-a77d-4549-8f67-6b51f7755a3e"],
         };
         axios
             .post(`${urlAPI}/api/v1/productService/getFilteredList`, querySportsPlans)
--- src/user/pages/sports-plans/pages/SportsPlansDetail.jsx
+++ src/user/pages/sports-plans/pages/SportsPlansDetail.jsx
@@ -1,8 +1,116 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, TouchableOpacity, View, ScrollView } from 'react-native'
+import { GlobalStyles } from '../../../../styles/GlobalStyles'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Text } from 'react-native';
+import { Card } from 'react-native-paper';
+import { Button } from "react-native-paper";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
-export const SportsPlansDetail = () => {
+export const SportsPlansDetail = ({ navigation, route }) => {
+    const { sportPlanDetails } = route.params;
     return (
-        <Text>hola</Text>
+        <View style={styles.sportApp}>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', width: '100%' }}>
+                <TouchableOpacity onPress={() => navigation.goBack()} >
+                    <Icon name="arrow-left" style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            <Text style={[styles.rutinaDeportiva]}>Rutina Deportiva</Text>
+            <Text style={[styles.sportPlanName]}>{sportPlanDetails.description}</Text>
+            <ScrollView>
+                {sportPlanDetails.trainingPlan.trainings.map((training, trainingIndex) => (
+                    <View key={trainingIndex}>
+                        <Text style={styles.trainings}>{training.name}</Text>
+                        {training.exercises.map((exercise, exerciseIndex) => (
+                            <View key={exerciseIndex}>
+                                <Text style={styles.exercise}>{exercise.name}</Text>
+                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', width: '100%' }}>
+
+                                    <Card style={[GlobalStyles.miniCard, { marginTop: 10, marginRight: 10 }]}>
+                                        <Card.Cover style={GlobalStyles.miniCardCover} source={{ uri: exercise.picture }} />
+                                    </Card>
+                                    <Text style={styles.smLetters}>{exercise.sets} series{'\n'}{exercise.repeats} repeticiones</Text>
+
+                                </View>
+                            </View>
+                        ))}
+                    </View>
+                ))}
+            </ScrollView>
+            <View style={{ alignItems: 'center' }}>
+                <Button
+                    style={styles.btnLarge}
+                    labelStyle={GlobalStyles.btnLayerStyle}
+                    contentStyle={GlobalStyles.btnLarge1}
+                    onPress={() => {
+
+                    }}
+                >
+                    Comenzar entrenamiento
+                </Button>
+            </View>
+        </View>
+
     )
 }
+const styles = StyleSheet.create({
+
+    icon: {
+        fontSize: 42,
+        color: '#EA9354',
+    },
+    btnLarge: {
+        marginBottom: 15
+    },
+    smLetters: {
+        color: '#000000',
+        fontSize: 14,
+        letterSpacing: 0,
+        fontWeight: '600',
+        lineHeight: 21,
+        marginTop: 5
+    },
+    sportPlanName: {
+        textAlign: 'center',
+        fontSize: 24,
+        fontWeight: "600",
+        color: '#000000',
+    },
+    trainings: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: "600",
+        color: '#000000',
+        marginTop: 25
+    },
+    rutinaDeportiva: {
+        textAlign: 'center',
+        marginTop: 20,
+        marginBottom: 5,
+        fontSize: 32,
+        fontWeight: "600",
+        color: '#000000',
+    },
+    sportApp: {
+        backgroundColor: "#fff",
+        flex: 1,
+        width: wp("100%"),
+        height: hp("100%"),
+        overflow: "hidden",
+        paddingTop: 60,
+        paddingLeft: 35,
+        paddingRight: 35,
+        paddingBottom: 10,
+    },
+    exercise: {
+        fontSize: 16,
+        fontWeight: "600",
+        color: '#000000',
+        marginTop: 15
+    }
+
+});
